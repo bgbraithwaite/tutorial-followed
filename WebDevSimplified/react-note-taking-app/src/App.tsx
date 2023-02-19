cc86b048a3354adf8665c8a94551baeb540d3dc6
Benjamin Braithwaite <478171+bgbraithwaite@users.noreply.github.com>
2023-02-19 06:20:29 -0500

    Added WebDevSimplified/react-note-taking-app

---
+++ WebDevSimplified/react-note-taking-app/src/App.tsx
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+  return <Routes>
+    <Route path="/" element={<h1>Home</h1>} />
+  </Routes>
+}
+
+export default App
